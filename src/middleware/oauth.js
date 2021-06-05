3626a97d5de9c6e5675139b55415f533c58a9bb6
AliBakerSartawi <alisartawi@live.com>
2021-06-05 14:06:22 +0300

    added organizational github secret and id & mongodb_uri

--- src/middleware/oauth.js
+++ src/middleware/oauth.js
@@ -2,83 +2,81 @@
 
 const superagent = require('superagent');
 const jwt = require('jsonwebtoken');
-const User = require('../models/user')
+const User = require('../models/user');
 
-const SECRET = 'mysecret' || process.env.SECRET ;
-const CLIENT_ID = 'a41277711942cc3f0f87' || process.env.CLIENT_ID;
-const CLIENT_SECRET = '6f34f4f2e62f83510bcbda767d07d099dff52e66' || process.env.ClIENT_SECRET;
+const SECRET = 'mysecret' || process.env.SECRET;
+const CLIENT_ID = '88070f9af0cd30ac368b' || process.env.CLIENT_ID;
+const CLIENT_SECRET =
+  '55e61054b00e7b4d8251ed4107816c9ca652cfbb' || process.env.ClIENT_SECRET;
 
 const tokenUrl = 'https://github.com/login/oauth/access_token';
 const userUrl = 'https://api.github.com/user';
 
 module.exports = async (req, res, next) => {
-    try{
-        const code = req.query.code
-        
-        const token = await exchangeCodeWithToken(code)
+  try {
+    const code = req.query.code;
 
-        let remoteUser = await exchangeTokenWithUserInfo(token)
+    const token = await exchangeCodeWithToken(code);
 
-        let [localUser, localToken] = await getLocalUser(remoteUser)
-        req.user = localUser;
-        req.token = localToken;
-        next()
-        
-    } catch(e) {
-        console.log(e.message);
-        next('INVALID LOGIN')
-    }
+    let remoteUser = await exchangeTokenWithUserInfo(token);
 
-
-}
-
+    let [localUser, localToken] = await getLocalUser(remoteUser);
+    req.user = localUser;
+    req.token = localToken;
+    next();
+  } catch (e) {
+    console.log(e.message);
+    next('INVALID LOGIN');
+  }
+};
 
 async function exchangeCodeWithToken(code) {
-    try {
-        const tokenRes = await superagent.post(tokenUrl).send({
-            client_id: CLIENT_ID,
-            client_secret: CLIENT_SECRET,
-            code: code
-        })
-        return tokenRes.body.access_token
-    } catch(e) {
-        console.log(e.message);
-    }
-} 
-
-
-async function exchangeTokenWithUserInfo(token) {
-    try {
-        const userInfo = await superagent.get(userUrl).set({
-            'Authorization': `token ${token}`,
-            'User-Agent': 'qaisw96/1.0'
-        })
-        // console.log('user ===>', userInfo.body);
-        return userInfo.body
-    }catch(e) {
-        console.log(e.message);
-    }
+  try {
+    const tokenRes = await superagent.post(tokenUrl).send({
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      code: code
+    });
+    return tokenRes.body.access_token;
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
+async function exchangeTokenWithUserInfo(token) {
+  try {
+    const userInfo = await superagent.get(userUrl).set({
+      Authorization: `token ${token}`,
+      'User-Agent': 'spaghettiCode'
+    });
+    // console.log('user ===>', userInfo.body);
+    return userInfo.body;
+  } catch (e) {
+    console.log(e.message);
+  }
+}
 
 async function getLocalUser(userObj) {
-    
-    try {
-        let userRecord = {
-            username: userObj.login,
-            password: 'oauth',
-            avatar_url: userObj.avatar_url,
-            bio: userObj.bio
-        }
-        
-        let token = jwt.sign({username: userRecord.username}, SECRET)
-        userRecord.token=token
-        // const check = jwt.verify(userRecord.token, SECRET)
-        let newUser = new User(userRecord)
-        let user = await newUser.save()
-
-        return [user, token]
-    } catch(e) {
-        console.log(e.message);
+  try {
+    let userRecord = {
+      username: userObj.login,
+      password: 'oauth',
+      avatar_url: userObj.avatar_url,
+      bio: userObj.bio
+    };
+    let token = jwt.sign({ username: userRecord.username }, SECRET);
+    userRecord.token = token;
+    const check = jwt.verify(userRecord.token, SECRET);
+    const result = await User.findOne({ username: check.username });
+    let user;
+    if (result) {
+      user = result;
+    } else {
+      let newUser = new User(userRecord);
+      user = await newUser.save();
     }
+    return [user, token];
+  } catch (e) {
+    console.log(e.message);
+  }
 }
